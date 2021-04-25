f4421137be112c5b126287e92cb4aff8924faca8
domfraise <domfraise@gmail.com>
2021-04-25 14:24:18 +0100

    Import openpost and plot basic skelton outline for photo

---
+++ opencv/python/openpose/CMakeFiles/pyopenpose.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/domfraise/dev/projects/openpose/python/openpose/openpose_python.cpp" "/home/domfraise/dev/projects/openpose/build/python/openpose/CMakeFiles/pyopenpose.dir/openpose_python.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "CPU_ONLY"
+  "USE_CAFFE"
+  "USE_CPU_ONLY"
+  "pyopenpose_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "caffe/include"
+  "../3rdparty/pybind11/include"
+  "/usr/include/python3.8"
+  "/home/linuxbrew/.linuxbrew/Cellar/opencv@3/3.4.14/include"
+  "/home/linuxbrew/.linuxbrew/Cellar/opencv@3/3.4.14/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/domfraise/dev/projects/openpose/build/src/openpose/CMakeFiles/openpose.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
